65a79094ddfdf3298b5af0745992abe1e18c3c99
Michał <mkpietrzyk@gmail.com>
2021-04-26 00:05:26 +0200

    Changes to gameManager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,9 +6,6 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public bool gameStart;
-    public bool gamePause;
-
     [SerializeField] private IntVariable balance;
     [SerializeField] private IntVariable cash;
     [SerializeField] private IntVariable cowsCount;
@@ -19,20 +16,16 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject houseContainer;
     [SerializeField] private GameObject treeContainer;
     
-
-    public enum MenuState
-    {
-        Pause,
-        End,
-        Main,
-        Settings,
-        Tutorial
-    }
-
-    public MenuState menuState;
+    
+    public BoolVariable gameStarted;
+    public BoolVariable gamePaused;
+    public BoolVariable gameEnded;
+    public StringVariable uiState;
+    public SetStringVariableValue setUIVisibility;
 
     void Start()
     {
+        uiState.Value = uiState.InitialValue;
         cowsCount.Value = 2;
         cash.Value = 100;
         balance.Value = 0;
@@ -46,15 +39,25 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!gamePaused.Value && gameStarted.Value && Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+
+        if (gamePaused.Value && Input.GetKeyDown(KeyCode.Escape))
+        {
+            ResumeGame();
+        }
     }
 
     private IEnumerator UpdateScore()
     {
-        while (!gamePause)
+        while (gameStarted.Value)
         {
             yield return new WaitForSeconds(5);
             int cowsHappiness = (int) (cowsCount.Value * 0.2f);
-            int currentBalance = householdsCount.Value * 20 + cowsCount.Value * cowsHappiness - cowsCount.Value * 5 - (int) connectionDistance.Value;
+            int cowsUnhapiness = (int) (householdsCount.Value / cowsCount.Value) * 10;
+            int currentBalance = householdsCount.Value * 20 + cowsCount.Value * cowsHappiness - cowsCount.Value * 5 - (int) connectionDistance.Value - cowsUnhapiness;
             balance.Value = currentBalance;
             cash.Value += currentBalance;
         }
@@ -87,5 +90,36 @@ public class GameManager : MonoBehaviour
             Vector3 randomPosition = new Vector3(randomX, yPos, randomZ);
             Instantiate(go, randomPosition, Quaternion.identity, parent.transform);
         }
+    }
+    
+    public void StartGame()
+    {
+        gameStarted.Value = true;
+        uiState.Value = "PlayerUI";
+    }
+
+    public void PauseGame()
+    {
+        gamePaused.Value = true;
+        Time.timeScale = 0;
+        uiState.Value = "PauseMenu";
+    }
+
+    public void ResumeGame()
+    {
+        gamePaused.Value = false;
+        Time.timeScale = 1;
+        uiState.Value = "PlayerUI";
+    }
+
+    public void ReturnToMenu()
+    {
+        gameStarted.Value = false;
+        uiState.Value = "MainMenu";
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
---
+++ Assets/Scripts/UI/UIElement.cs
@@ -0,0 +1,28 @@
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+public class UIElement : MonoBehaviour
+{
+    public StringVariable uiState;
+    public StringConstant uiElement;
+
+    public void SetVisibility()
+    {
+        if (uiState.Value == uiElement.Value)
+        {
+            SetChildrenActive(true);
+        }
+        else
+        {
+            SetChildrenActive(false);
+        }
+    }
+
+    private void SetChildrenActive(bool value)
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(value);
+        }
+    }
+}
