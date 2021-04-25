ab5be289eb1192f5db5068efd2622341bbf04c39
Michał <mkpietrzyk@gmail.com>
2021-04-25 02:14:49 +0200

    Game Logic loop completed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -8,13 +9,13 @@ public class GameManager : MonoBehaviour
     public List<GameObject> connections;
     public List<GameObject> households;
 
-    public int cowsCount;
-
-    public int cash;
-
     public bool gameStart;
     public bool gamePause;
 
+    [SerializeField] private IntVariable score;
+    [SerializeField] private IntVariable cash;
+    [SerializeField] private IntVariable cowsCount;
+    [SerializeField] private IntVariable householdsCount;
     public enum MenuState
     {
         Pause,
@@ -27,7 +28,7 @@ public class GameManager : MonoBehaviour
     public MenuState menuState;
     void Start()
     {
-        
+        StartCoroutine(UpdateScore());
     }
 
     // Update is called once per frame
@@ -35,4 +36,15 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    private IEnumerator UpdateScore()
+    {
+        while (!gamePause)
+        {
+            yield return new WaitForSeconds(5);
+            int cashAdjustment = householdsCount.Value * 10 - cowsCount.Value * 5;
+            cash.Value += cashAdjustment;
+            score.Value = cashAdjustment * 1000;
+        }
+    } 
 }
